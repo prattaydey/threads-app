596aa359b9a1b66048dccef47f2b75f7286a500e
Prattay Dey <prattaydey@Prattays-MacBook-Pro.local>
2024-05-31 04:07:30 -0400

    3.4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Container maxWidth="620px">
       <Header />
       <Routes>
-        <Route path ="/:username" element={ <UserPage /> } />
-        <Route path ="/:username/post/:pid" element={ <PostPage /> } />
+        <Route path="/" element={ <HomePage /> } />
+        {/* <Route path="/auth" element={ <AuthPage /> } /> */}
+
+        <Route path="/:username" element={ <UserPage /> } />
+        <Route path="/:username/post/:pid" element={ <PostPage /> } />
       </Routes>
     </Container>
   );
